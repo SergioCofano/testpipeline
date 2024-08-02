f4640e5dc807c81439b6827514ac44505082969b
Sergio Giuseppe Cofano <152389167+SergioCofano@users.noreply.github.com>
2024-08-02 10:47:44 +0200

    Update antonio

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline! Freestyle project! Ciao angela</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declarative pipeline! Freestyle project! Ciao antonio</p>'; ?> 
  </body>
 </html>
