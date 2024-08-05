d22a88dd00a97248a8f6f789519a53ec22c1658c
Sergio Giuseppe Cofano <152389167+SergioCofano@users.noreply.github.com>
2024-08-05 17:11:45 +0200

    Update UTENTI

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline!! Ciao UTENTE</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declarative pipeline!! Ciao UTENTI</p>'; ?> 
  </body>
 </html>
